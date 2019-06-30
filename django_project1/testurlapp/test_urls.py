490aeaaeb0857dbc9fd36b003c814da740958b39
Khristoforov Ilya <hristoforov.ilya@gmail.com>
2019-06-30 17:35:09 +0300

    create url /test_app/user/12/2000/ for app and send third parameter ('year')

--- django_project1/testurlapp/test_urls.py
+++ django_project1/testurlapp/test_urls.py
@@ -2,5 +2,6 @@ from django.urls import path, re_path
 from testurlapp import views
 
 urlpatterns = [
-	re_path(r'^user/(\d+)/$', views.home, name='home'),
+	#site.com/user/12/2000/
+	re_path(r'^user/(\d{2})/(\d{4})/$', views.home, name='home'),
 ]
--- django_project1/testurlapp/views.py
+++ django_project1/testurlapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request, month):
+def home(request, month, year):
 	return render(request, 'home.html', {})
