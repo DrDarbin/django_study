88119432e6d32c890c4a307c86dc644dbfb6cadd
Khristoforov Ilya <hristoforov.ilya@gmail.com>
2019-06-30 13:56:24 +0300

    rename models, order items in models

--- django_project1/firstapp/models.py
+++ django_project1/firstapp/models.py
@@ -7,6 +7,11 @@ class PizzaShop(models.Model):
 	description = models.TextField(verbose_name = 'Описание')
 	rating = models.FloatField(default = 0, verbose_name = 'Рейтинг')
 	url = models.URLField(verbose_name = 'Интернет-адрес пицерии')
+	
+	class Meta:
+		verbose_name = 'Пицерия'
+		verbose_name_plural = 'Пицерии'
+		ordering = ['name']
 
 	def __str__(self):
 		return(self.name)
@@ -16,6 +21,11 @@ class Pizza(models.Model):
 	name = models.CharField(max_length = 30, verbose_name = 'Название пиццы')
 	short_description = models.CharField(max_length = 30, verbose_name  = 'Краткое описание')
 	price = models.IntegerField(default = 0, verbose_name = 'Цена')
+	
+	class Meta:
+		verbose_name = 'Пицца'
+		verbose_name_plural = 'Пиццы'
+		ordering = ['price']
 
 	def __str__(self):
 		return self.name
