2706ec2acac76e0734d19df4602b81bf70b971df
Khristoforov Ilya <hristoforov.ilya@gmail.com>
2019-06-30 17:18:31 +0300

    Create test url for app (controller) (/test_app)

--- django_project1/django1/urls.py
+++ django_project1/django1/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from testurlapp import views
+from django.urls import path, include
+#from testurlapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('', views.home, name='home'),
+	#path('', views.home, name='home'),
+	path('test_app/', include('testurlapp.test_urls')),
 ]
